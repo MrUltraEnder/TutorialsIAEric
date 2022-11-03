6c28c3bb39d3b7521fb8e8f0e639edd4d8a8324e
Eric Zaleta <ericzaleta@gmail.com>
2022-11-03 15:02:00 +0000

    Time first step

--- Assets/Script/UpdateMove.cs
+++ Assets/Script/UpdateMove.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class UpdateMove : MonoBehaviour
 {
-
+    float speed = 2;
     void Update()
     {
-
+        this.transform.Translate(0, 0, Time.deltaTime * speed);
     }
 }
