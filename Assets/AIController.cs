b02a3b58de8510f1414ab16242061a39eac56981
Eric Zaleta <ericzaleta@gmail.com>
2022-12-02 18:40:00 +0000

    Creating a City Crowd Part 1

--- Assets/AIController.cs
+++ Assets/AIController.cs
@@ -5,12 +5,28 @@ using UnityEngine.AI;
 
 public class AIController : MonoBehaviour
 {
-    public GameObject goal;
+    GameObject[] goalLocations;
     NavMeshAgent agent;
+    Animator anim;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(goal.transform.position);
+        goalLocations = GameObject.FindGameObjectsWithTag("Goal");
+        int i = Random.Range(0, goalLocations.Length);
+        agent.SetDestination(goalLocations[i].transform.position);
+        anim = GetComponent<Animator>();
+        anim.SetTrigger("isWalking");
+    }
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    private void Update()
+    {
+        if (agent.remainingDistance < 0.5f)
+        {
+            int i = Random.Range(0, goalLocations.Length);
+            agent.SetDestination(goalLocations[i].transform.position);
+        }
     }
 }
